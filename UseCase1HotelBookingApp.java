57d11b0469d107527132822dd6fbdaae26002a66
sb2165-hue <sb2165@srmist.edu.in>
2026-03-23 09:46:14 +0530

    UC1 completed

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,12 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ * @author Alwyn
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+    }
+}
